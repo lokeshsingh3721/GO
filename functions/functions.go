ef2f6442d14cce025a69d03360e1bc531995b838
lokeshsingh3721 <lokesh3721@gmail.com>
2024-03-13 19:29:52 +0530

    learn functions

--- functions/functions.go
+++ functions/functions.go
@@ -1,16 +1,37 @@
 package main
 
-import "fmt"
-
-func main(){
-
-	var ans int = sum(10,12)
-
-	fmt.Printf("ans is %d",ans)
-
-}
+import (
+	"fmt"
+)
 
 
 func sum(a int , b int ) int   {
 	return a + b;
 }
+
+// function can take another function as an arguement
+
+func child (b string) string {
+	return b;
+}
+
+func parent (a  string ,child func (string) string) string {
+	res:=child(a);
+	return res
+}
+
+
+func main(){
+
+	// var ans int = sum(10,12)
+
+	// fmt.Printf("ans is %d",ans)
+
+fmt.Printf(parent("lokesh",child))
+
+	
+
+}
+
+
+
