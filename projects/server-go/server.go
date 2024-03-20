8cd30ff30bc7dab437a2116733f0b862b7b06685
lokeshsingh3721 <lokesh3721@gmail.com>
2024-03-20 21:55:26 +0530

    add GET and POST method

--- projects/server-go/server.go
+++ projects/server-go/server.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 
+type Todo struct {
+	ID   int
+	Task string
+}
+
+var todos []Todo
+
 func main() {
 
-	http.HandleFunc("/",func(w http.ResponseWriter , r *http.Request){
-		w.Write([]byte("hell world"))
+	http.HandleFunc("/",func(rw http.ResponseWriter , r *http.Request){
+
+		switch r.Method {
+		case "GET":
+			json.NewEncoder(rw).Encode(todos)
+		case "POST":
+			// create variable to save todo
+			var todo Todo
+			// decode `r.Body` `r *http.Request` to get data from request
+			// decode the result to todo
+			json.NewDecoder(r.Body).Decode(&todo)
+			// append new todo in todos
+			todos = append(todos, todo)
+			// finally, encode again create the resutl with json format
+			json.NewEncoder(rw).Encode(todo)
+		}
 	})
 
-	
-
-	log.Fatal(http.ListenAndServe(":8000",nil))
+	log.Fatal(http.ListenAndServe(":8080",nil))
 	
 }
